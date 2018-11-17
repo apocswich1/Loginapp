ad5a74210fbf3aea3fb9588f62922e3651e8b9bd
apocswich1 <ejhernandezj@gmail.com>
2018-11-17 18:39:32 -0300

    segundo commit validacion de formulario

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,32 @@ router.get('/login', function(req, res){
     res.render('login')
 })
 
+//register user
+router.post('/register', function(req, res){
+    var name = req.body.name;
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var cpassword = req.body.cpassword;
+
+    req.checkBody('name','Name is required').notEmpty();
+    req.checkBody('username','Username is required').notEmpty();
+    req.checkBody('email','Email is required').notEmpty();
+    req.checkBody('email','Verify Email format').notEmpty();
+    req.checkBody('password','Password is required').notEmpty();
+    req.checkBody('cpassword','Password do not match').equals(req.body.password);
+    req.checkBody('cpassword','Second password is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render("register",{
+            errors: errors
+        });
+    }else{
+        console.log("PASSED")
+    }
+})
 
 
 module.exports = router
