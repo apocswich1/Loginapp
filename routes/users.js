6f9d6444a2c41bf7ec215cca5ac05d47d7e468fe
apocswich1 <ejhernandezj@gmail.com>
2018-11-17 22:31:11 -0300

    Completado logueo

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,23 @@
 var express = require('express')
 var router = express.Router()
 var User = require('../models/user')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+
+//ensure authenticated
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }else{
+        //req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
+
+//register
+router.get('/', ensureAuthenticated, function(req, res){
+    res.render('index')
+})
 
 //register
 router.get('/register', function(req, res){
@@ -10,6 +27,13 @@ router.get('/register', function(req, res){
 //login
 router.get('/login', function(req, res){
     res.render('login')
+})
+
+//login
+router.get('/logout', function(req, res){
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login')
 })
 
 //register user
@@ -52,6 +76,42 @@ router.post('/register', function(req, res){
         console.log("PASSED")
     }
 })
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+     User.getUserByUsername(username, function(err,user){
+        if(err) throw err;
+        if(!user){
+            return done(null,false,{message:'Unknown user'})
+        }
+        User.comparePassword(password, user.password, function(err, isMatch){
+            if(err) throw err;
+            if(isMatch){
+                return done(null, user);
+            }else{
+                return done(null, false, {message: 'Invalid Password'});
+            }
+            
+        })
+     });   
+    }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.getUserById(id, function(err, user) {
+          done(err, user);
+        });
+      });
+
+
+router.post('/login',
+  passport.authenticate('local',{successRedirect:'/',failureRedirect:'/users/login',failureFlash:true}),
+  function(req, res) {
+    res.redirect('/')
+  });
 
 
 module.exports = router
